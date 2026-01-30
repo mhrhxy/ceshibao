96f15725ca1e67003003984779a144f769ef923a
mahaorui <mhr699112@foxmail.com>
2026-01-30 08:48:09 +0800

    提升iOS最低版本到13.0，修复插件兼容问题

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,8 @@
 import UIKit
 import Flutter
 import webview_flutter_wkwebview // 导入 WebView 依赖
-
+import KakaoSDKCommon // 导入 Kakao SDK 依赖
+import flutter_naver_login // 导入 Naver 登录插件
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -10,6 +11,8 @@ import webview_flutter_wkwebview // 导入 WebView 依赖
   ) -> Bool {
     let controller = window?.rootViewController as! FlutterViewController
     let webViewPlugin = WebViewFlutterPlugin()
+    KakaoSDK.initSDK(appKey: "kakaoca610cfd836872a2e451f79a1be06cf6") // 初始化 Kakao SDK
+    NaverLoginPlugin.register(with: controller.registrar(forPlugin: "flutter_naver_login")) // 注册 Naver 登录插件
     webViewPlugin.setup(with: controller.flutterEngine) // 注册 WebView 插件
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
